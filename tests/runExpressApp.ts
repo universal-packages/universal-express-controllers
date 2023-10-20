81837a6bc4798ca0c18e8dbaa795575b06482f21
David De Anda <omarandstuff@gmail.com>
2023-10-20 01:08:50 -0600

    runExpressApp util file

---
+++ tests/runExpressApp.ts
@@ -0,0 +1,25 @@
+import { ExpressApp } from '../src'
+
+let currentApp: ExpressApp
+
+declare global {
+  function runExpressApp(appLocation?: string, eventListener?: any, debugError?: boolean): Promise<ExpressApp>
+}
+
+global.runExpressApp = async function setAppLocation(appLocation: string = './tests/__fixtures__', eventListener?: any, debugError?: boolean): Promise<ExpressApp> {
+  currentApp = new ExpressApp({ appLocation, port: fDefaultPort })
+
+  if (eventListener) currentApp.on('*', eventListener)
+  if (debugError) currentApp.on('request/error', console.log)
+
+  await currentApp.prepare()
+  await currentApp.run()
+
+  return currentApp
+}
+
+afterEach(async (): Promise<void> => {
+  if (currentApp) currentApp.stop()
+
+  currentApp = undefined
+})
--- tests/setup.ts
+++ tests/setup.ts
@@ -1,31 +1,7 @@
-import { ExpressApp } from '../src'
+import './runExpressApp'
 
 // node > 19 has some issues with fetch closing sockets on consecutive requests
 if (process.env.CI || process.versions.node.startsWith('20')) {
   jest.retryTimes(5)
   jest.setTimeout(10000)
 }
-
-let currentApp: ExpressApp
-
-declare global {
-  function runExpressApp(appLocation?: string, eventListener?: any, debugError?: boolean): Promise<ExpressApp>
-}
-
-global.runExpressApp = async function setAppLocation(appLocation: string = './tests/__fixtures__', eventListener?: any, debugError?: boolean): Promise<ExpressApp> {
-  currentApp = new ExpressApp({ appLocation, port: fDefaultPort })
-
-  if (eventListener) currentApp.on('*', eventListener)
-  if (debugError) currentApp.on('request/error', console.log)
-
-  await currentApp.prepare()
-  await currentApp.run()
-
-  return currentApp
-}
-
-afterEach(async (): Promise<void> => {
-  if (currentApp) currentApp.stop()
-
-  currentApp = undefined
-})
